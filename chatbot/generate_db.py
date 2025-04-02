e3f2b99fde2dc9eb831193aed760cbb4175a2b8e
osyonohito <164350891+osyonohito@users.noreply.github.com>
2025-04-02 22:40:38 +0900

    Add files via upload

--- chatbot/generate_db.py
+++ chatbot/generate_db.py
@@ -1,7 +1,7 @@
 import os
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
 # テキストファイルのパス
