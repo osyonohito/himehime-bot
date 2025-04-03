2fd850b2b7493d2d5bce7777f066cebe6e2ebafe
osyonohito <164350891+osyonohito@users.noreply.github.com>
2025-04-04 00:15:30 +0900

    Add files via upload

--- chatbot/rag_chain.py
+++ chatbot/rag_chain.py
@@ -1,4 +1,4 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
